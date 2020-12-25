56f72d39254cd4a96fa333a75d58a84f6044c3e8
ZakirovArtem <meatgamer@yandex.ru>
2020-12-25 12:01:12 +0700

    relese.v1

---
+++ app/src/main/java/com/example/zakirov_v_6/BackActivity.java
@@ -0,0 +1,14 @@
+package com.example.zakirov_v_6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BackActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_back);
+    }
+}
